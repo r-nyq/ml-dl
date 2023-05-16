49b8e8be197d6e4075be0adee846c1c72f13bf78
Robert Nyquist <robert.nyquist@smartr.se>
2023-05-16 14:07:13 +0200

    Formating

--- model_service.py
+++ model_service.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 MODEL = load("price_model.joblib")
 
+
 @app.route("/predict_price/<int:area>", methods=["GET"])
 def predict_house_price(area):
     prediction = MODEL.predict([[area]])
     return jsonify({"price": prediction[0]})
 
+
 if __name__ == "__main__":
-    with app.app_context():
-        app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
